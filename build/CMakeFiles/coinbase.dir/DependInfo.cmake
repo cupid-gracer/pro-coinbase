1498c92290e6207b26ccbaf872f77d4b1d538499
cupid-gracer <cupidgracer@gmail.com>
2021-01-29 05:32:10 -0800

    delete Util.cpp

---
+++ build/CMakeFiles/coinbase.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cupid/work/pro-coinbase/src/API.cpp" "/home/cupid/work/pro-coinbase/build/CMakeFiles/coinbase.dir/src/API.cpp.o"
+  "/home/cupid/work/pro-coinbase/src/App.cpp" "/home/cupid/work/pro-coinbase/build/CMakeFiles/coinbase.dir/src/App.cpp.o"
+  "/home/cupid/work/pro-coinbase/src/Auth.cpp" "/home/cupid/work/pro-coinbase/build/CMakeFiles/coinbase.dir/src/Auth.cpp.o"
+  "/home/cupid/work/pro-coinbase/src/Util.cpp" "/home/cupid/work/pro-coinbase/build/CMakeFiles/coinbase.dir/src/Util.cpp.o"
+  "/home/cupid/work/pro-coinbase/src/Websock.cpp" "/home/cupid/work/pro-coinbase/build/CMakeFiles/coinbase.dir/src/Websock.cpp.o"
+  "/home/cupid/work/pro-coinbase/src/main.cpp" "/home/cupid/work/pro-coinbase/build/CMakeFiles/coinbase.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CPPREST_FORCE_HTTP_CLIENT_ASIO"
+  "CPPREST_FORCE_HTTP_LISTENER_ASIO"
+  "CPPREST_NO_SSL_LEAK_SUPPRESS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../$(cpprestsdk_DIR)"
+  "/usr/include/crypto++"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/coinbase.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coinbase.dir/src/API.cpp.o"
+  "CMakeFiles/coinbase.dir/src/App.cpp.o"
+  "CMakeFiles/coinbase.dir/src/Auth.cpp.o"
+  "CMakeFiles/coinbase.dir/src/Util.cpp.o"
+  "CMakeFiles/coinbase.dir/src/Websock.cpp.o"
+  "CMakeFiles/coinbase.dir/src/main.cpp.o"
+  "coinbase"
+  "coinbase.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coinbase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
